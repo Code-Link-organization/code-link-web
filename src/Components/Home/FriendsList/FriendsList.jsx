bf3cdde933660ec80613e98cdbb30cc714acbee4
BasharElmetwaliahmed <belmetwali@std.mans.edu.eg>
2023-09-16 16:52:57 +0200

    enhance home components formatte

--- src/Components/Home/FriendsList/FriendsList.jsx
+++ src/Components/Home/FriendsList/FriendsList.jsx
@@ -1,15 +1,11 @@
-import MyFriends from "./MyFriends/MyFriends"
-import Requests from "./Requests/Requests"
+import Requests from './Requests/Requests';
 
-function FriendsList({isFixed}) {
+function FriendsList({ isFixed }) {
   return (
-    <div className="w-1/4">
-    <div className={`w-full  ${!isFixed?'':'fixed !w-1/4 top-0 transform    '} `}>
-    <Requests/>
+    <div className={`w-1/4 ${isFixed ? 'fixed !w-1/4 top-0 transform' : ''}`}>
+      <Requests />
     </div>
-    </div>
-
-  )
+  );
 }
 
-export default FriendsList
+export default FriendsList;
--- src/Components/Home/FriendsList/Requests/RequestItem.jsx
+++ src/Components/Home/FriendsList/Requests/RequestItem.jsx
@@ -1,22 +1,28 @@
-import { imgLink } from '../../../../api'
-import { transferDate } from '../../../../Functions/transferDate'
-import RespondToInvite from './RespondToInvite'
-function RequestItem({request}) {
+/* eslint-disable react/prop-types */
+import { imgLink } from '../../../../api';
+import { transferDate } from '../../../../Functions/transferDate';
+import RespondToInvite from './RespondToInvite';
+
+function RequestItem({ request }) {
   return (
-    <div className='flex justify-between items-center py-2 '>
-        <div className='flex gap-4'>
-         <img src={`${imgLink}/${request.team.imageUrl}`} alt='profile img' className='align-middle justify-self-center w-14 h-14 rounded-full'/>
-         <div>
-          <h5 className='font-semibold text-sm mb-3'>you are invited to join {request.team.name}</h5>
-          <p className='text-xs'>{transferDate(request.created_at)}</p>
-         </div>
+    <div className="flex justify-between items-center py-2">
+      <div className="flex gap-4">
+        <img
+          src={`${imgLink}/${request.team.imageUrl}`}
+          alt="Profile Image"
+          className="align-middle justify-self-center w-14 h-14 rounded-full"
+        />
+        <div>
+          <h5 className="font-semibold text-sm mb-3">
+            You are invited to join {request.team.name}
+          </h5>
+          <p className="text-xs">{transferDate(request.created_at)}</p>
         </div>
-                <RespondToInvite request={request} response='accept'/>
-                <RespondToInvite request={request} response='reject'/>
-
-
+      </div>
+      <RespondToInvite request={request} response="accept" />
+      <RespondToInvite request={request} response="reject" />
     </div>
-  )
+  );
 }
 
-export default RequestItem
+export default RequestItem;
